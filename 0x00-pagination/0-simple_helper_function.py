cfd0fdf1543f8a72ffde88d199e546f19c576eb9
Frimpong rijkaard <frimpongrijkaard94@yahoo.com>
2024-10-28 23:29:33 +0000

    task on pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 """Simple helper function that take two inputs
 """
 from typing import Tuple
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """function should return a tuple of size 
     two containing a start index and an end index corresponding 
